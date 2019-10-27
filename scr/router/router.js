37d4f1eeea2fd99baaee200588f2341d83ddafc6
cor <1057376155@qq.com>
2019-10-27 11:38:20 +0800

    test

--- scr/router/router.js
+++ scr/router/router.js
@@ -1,47 +1,74 @@
-import * as React from 'react';
-import { Button, View, Text } from 'react-native';
+// import {createStackNavigator, createAppContainer} from 'react-navigation'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import routerConfig from './routerConfig';
+import {
+    StyleSheet,
+    SafeAreaView,
+    Text,
+    View,
+    Easing,
+    TouchableOpacity,
+    Animated
+} from 'react-native';
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
+function generateRoute(path, screen) {
+  return {
+    path,
+    screen
   }
 }
+var duration=routerConfig.defaultDuration
+const stackNavigate = createStackNavigator(
+  routerConfig.routers,
+  {
+  initialRouteName: 'page1',
+  headerMode: 'none',
+  mode: 'card',
+  defaultNavigationOptions: {
+    gesturesEnabled: false,
+  },
+  onTransitionStart(transitionProps,prevTransitionProps){
+    var name=transitionProps.scene.route.routeName
+    var preName=prevTransitionProps.scene.route.routeName
+    if(routerConfig.routers[name].duration>-1&&routerConfig.routers[preName].duration>-1){
+      //如果前后两个页面的过渡动画为0，则没有过渡动画
+      duration=routerConfig.routers[name].duration
+    }else{
+      duration=routerConfig.defaultDuration
+    }
 
-class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details')}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
-  }
-}
+  },
+  onTransitionEnd(){
+    duration=routerConfig.defaultDuration;
+  },
+  transitionConfig: () => ({
+    transitionSpec: {
+      duration: duration,
+      easing: Easing.bezier(.44,.45,.65,.95),
+      timing: Animated.timing,
+    },
+    screenInterpolator: sceneProps => {
+      const { layout, position, scene } = sceneProps;
+      const { index } = scene;
 
-const RootStack = createStackNavigator({
-  Home: HomeScreen,
-  Details: DetailsScreen,
-});
+      const height = layout.initHeight;
+      const translateX = position.interpolate({
+        inputRange: [index - 1, index, index + 1],
+        outputRange: [height, 0, 0],
+      });
 
-export default createAppContainer(RootStack);
+      const opacity = position.interpolate({
+        inputRange: [index - 1, index - 0.99, index],
+        outputRange: [0, 1, 1],
+      });
+
+      return { opacity, transform: [{ translateX }] };
+    },
+  }),
+})
+
+const Router = createAppContainer(stackNavigate)
+
+export default Router
+// export default createAppContainer(AppNavigator);
---
+++ scr/router/routerConfig.js
@@ -0,0 +1,9 @@
+import  page1 from '../pages/page1/page1';
+
+var routerConfig={
+    defaultDuration:400,//默认转场熟读
+    routers:{
+        page1:{screen:page1},
+    }
+}
+export default routerConfig
